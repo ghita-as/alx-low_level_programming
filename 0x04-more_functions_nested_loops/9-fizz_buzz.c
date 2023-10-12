6892cae1e09e247dec2c7c0af964b7b06146926b
ghita-as <ghita.as.amranisouhli@gmail.com>
2023-10-12 10:09:39 -0700

    0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -4,12 +4,6 @@
 /**
  * main - prints the numbers from 1 to 100, followed by a new line
  * but for multiples of three prints Fizz instead of the number
-#include "main.h"
-#include <stdio.h>
-
-/**
- * main - prints the numbers from 1 to 100, followed by a new line
- * but for multiples of three prints Fizz instead of the number
  * and for the multiples of five prints Buzz
  * Return: Always 0 (Success)
  */
